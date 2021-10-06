4a387ae6d1de470035bc386fa2905c00c560b89a
junha <terajunha@snu.ac.kr>
2021-10-06 11:02:53 +0900

    airkorea에서 한국 자료 가지고 오기

---
+++ getStationName.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+Encoding = 'TTb2xvR9D%2BEc9ydhumshJAa50AiEKEXPJNLuAUuMnU2P0oKHfE8xaZKcojj0Frp2IUg1Tkn1FTpHTB%2FiQKbxbA%3D%3D'
+Decoding = 'TTb2xvR9D+Ec9ydhumshJAa50AiEKEXPJNLuAUuMnU2P0oKHfE8xaZKcojj0Frp2IUg1Tkn1FTpHTB/iQKbxbA=='
+
+given_url = 'http://apis.data.go.kr/B552584/MsrstnInfoInqireSvc/getMsrstnList?numOfRows=608&serviceKey='+Encoding
+
+def getInfo(url) :
+    Stations = []
+    latitudes = []
+    longitudes = []
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text,"html.parser")
+
+    nameList = soup.findAll('stationname')
+    latList = soup.findAll('dmx')
+    lonList = soup.findAll('dmy')
+
+    for i in range(0,len(nameList)) :
+        Stations.append(nameList.__getitem__(i).get_text())
+        latitudes.append(latList.__getitem__(i).get_text())
+        longitudes.append(lonList.__getitem__(i).get_text())
+
+    StationsInfo=pd.DataFrame(index=Stations,data={"Lat":latitudes,"Lon":longitudes})
+    StationsInfo.index.name='Station'
+    # print(StationsInfo)
+    return StationsInfo
+
+getInfo(given_url)
